501e34cfc45712d7642957f828ca02d31286a047
Wade Green <thewadegreen@gmail.com>
2025-11-21 22:31:27 -0500

    Handle json and form requests

--- request/parse_request.go
+++ request/parse_request.go
@@ -43,32 +43,59 @@ type ProcessedRequest struct {
 	Headers map[string]string
 
 	// GET requests have Params
-	Params map[string]string
+	Params map[string]interface{}
 
 	// POST requests have Body and Data
 	Body string
 	Data map[string]interface{}
 }
 
+func parseKeyValuePairs(params string) map[string]interface{} {
+	paramsMap := make(map[string]interface{})
+	paramParts := strings.Split(params, "&")
+	for i := 0; i < len(paramParts); i++ {
+		keyValue := strings.SplitN(paramParts[i], "=", 2)
+		key, value := keyValue[0], keyValue[1]
+		paramsMap[key] = value
+	}
+	return paramsMap
+}
+
+func parseQueryParams(endpoint string) map[string]interface{} {
+	endpointPartsList := strings.SplitN(endpoint, "?", 2)
+	if len(endpointPartsList) < 2 {
+		log.Printf("Error parsing query params: %v", endpointPartsList)
+		return map[string]interface{}{}
+	}
+	paramsString := endpointPartsList[1]
+	return parseKeyValuePairs(paramsString)
+}
+
 func processGetRequest(request RawRequest) ProcessedRequest {
 	// Separate Headers from Body and Data
 	headerCount := len(request.Data) - 1
 	headers := request.Data[:headerCount]
-	body := request.Data[headerCount]
 
 	// Parse Headers
 	headersMap := parseHeaders(headers)
+
+	// Parse query params
+	paramsMap := parseQueryParams(request.Meta.endpoint)
+
 	return ProcessedRequest{
 		Meta:    request.Meta,
-		Params:  map[string]string{},
+		Params:  paramsMap,
 		Headers: headersMap,
-		Body:    body,
 	}
 }
 
 func parseHeaders(headers []string) map[string]string {
 	headersMap := make(map[string]string)
 	for i := 0; i < len(headers); i++ {
+		// Skip blank lines that are processed as headers
+		if len(headers[i]) == 0 {
+			continue
+		}
 		parts := strings.SplitN(headers[i], ":", 2)
 		if len(parts) != 2 {
 			log.Println("Invalid header:", headers[i])
@@ -81,6 +108,7 @@ func parseHeaders(headers []string) map[string]string {
 }
 
 func processPostRequest(request RawRequest) ProcessedRequest {
+	log.Printf("Processing request: %+v", request)
 	// Separate Headers from Body and Data
 	headerCount := len(request.Data) - 2
 	headers := request.Data[:headerCount]
@@ -90,20 +118,50 @@ func processPostRequest(request RawRequest) ProcessedRequest {
 	// Parse Headers
 	headersMap := parseHeaders(headers)
 
-	// Parse JSON Data payload
-	jsonData := make(map[string]interface{})
-	err := json.Unmarshal([]byte(data), &jsonData)
-	if err != nil {
-		log.Println("Failed to parse request: ", err)
-		return ProcessedRequest{}
+	if len(data) == 0 {
+		return ProcessedRequest{
+			Meta:    request.Meta,
+			Headers: headersMap,
+			Body:    body,
+		}
+	}
+
+	var contentType string
+	for key, value := range headersMap {
+		if key == "Content-Type" {
+			contentType = value
+		}
+	}
+
+	switch contentType {
+	case "application/json":
+		jsonData := make(map[string]interface{})
+		err := json.Unmarshal([]byte(data), &jsonData)
+		if err != nil {
+			log.Println("Failed to parse request: ", err)
+			return ProcessedRequest{}
+		}
+
+		return ProcessedRequest{
+			Meta:    request.Meta,
+			Headers: headersMap,
+			Body:    body,
+			Data:    jsonData,
+		}
+	case "x-www-form-urlencoded":
+		formData := parseKeyValuePairs(data)
+		return ProcessedRequest{
+			Meta:    request.Meta,
+			Headers: headersMap,
+			Body:    body,
+			Data:    formData,
+		}
 	}
 
 	return ProcessedRequest{
 		Meta:    request.Meta,
-		Params:  map[string]string{},
 		Headers: headersMap,
 		Body:    body,
-		Data:    jsonData,
 	}
 }
 
